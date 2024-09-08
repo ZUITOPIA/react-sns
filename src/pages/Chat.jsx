9ddbb9fa5ddd018129b58a88ba99d7889bce3370
ZUITOPIA <korhwngml123@naver.com>
2024-09-08 23:53:46 +0900

    feat: 컴포넌트 나누고 폴더에 위치

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,9 +1,18 @@
 import styled from '@emotion/styled';
+import ChatHeader from '../components/Chat/ChatHeader';
+import ChatBody from '../components/Chat/Chatbody';
 
 export default function Chat() {
   return (
     <>
-      <Style.Wrapper>채팅</Style.Wrapper>
+      <Style.Wrapper>
+        <Style.Header>
+          <ChatHeader />
+        </Style.Header>
+        <Style.Body>
+          <ChatBody />
+        </Style.Body>
+      </Style.Wrapper>
     </>
   );
 }
@@ -12,6 +21,14 @@ const Style = {
   Wrapper: styled.div`
     width: 430px;
     height: 1134px;
-    border: 1px solid red;
+    border: 1px solid lightgray;
+  `,
+  Header: styled.div`
+    padding: 22px;
+    border-bottom: 1px solid lightgray;
+  `,
+  Body: styled.div`
+    width: 92.5%;
+    padding: 22px;
   `,
 };
