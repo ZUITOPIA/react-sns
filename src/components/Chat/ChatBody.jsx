298c24e3eb93eac2bab800bf4b98f7886a3431cc
ZUITOPIA <korhwngml123@naver.com>
2024-09-10 20:50:30 +0900

    style: 기본 스타일 구현중

--- src/components/Chat/ChatBody.jsx
+++ src/components/Chat/ChatBody.jsx
@@ -6,28 +6,51 @@ export default function ChatBody({ messages, users }) {
 
         return (
           <>
-            {!message.isSender ? (
-              <div style={{}}>
-                <img
-                  style={{ width: '20px' }}
-                  src="/dlwlrma.jpg"
-                  alt="sender?.userName"
-                />
-              </div>
-            ) : (
-              <div>
-                <img
-                  style={{ width: '20px' }}
-                  src="/zuitopia.jpeg"
-                  alt="sender?.userName"
-                />
-              </div>
-            )}
+            <div
+              style={{
+                display: 'flex',
+                justifyContent: message.isSender ? 'flex-end' : 'flex-start',
+              }}
+            >
+              <div
+                style={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: message.isSender ? 'flex-end' : 'flex-start',
+                  maxWidth: '60%',
+                  padding: '12px',
+                  borderRadius: message.isSender
+                    ? '20px 4px 20px 24px'
+                    : '4px 20px 20px 24px',
+                  backgroundColor: message.isSender ? '#6245ff' : '#e9e9e9',
+                }}
+              >
+                {!message.isSender ? (
+                  <img
+                    style={{
+                      width: '32px',
+                      height: '32px',
+                      borderRadius: '50%',
+                    }}
+                    src={`/${sender?.profilePicture}`}
+                    alt={sender?.userName}
+                  />
+                ) : (
+                  <img
+                    style={{
+                      width: '32px',
+                      height: '32px',
+                      borderRadius: '50%',
+                    }}
+                    src={`/${sender?.profilePicture}`}
+                    alt={sender?.userName}
+                  />
+                )}
 
-            <div>
-              <div>{sender?.userId}</div>
-              <div>{message.content}</div>
-              <div>{new Date(message.timestamp).toLocaleTimeString()}</div>
+                <span>{sender?.userId}</span>
+                <div>{message.content}</div>
+                <div>{new Date(message.timestamp).toLocaleTimeString()}</div>
+              </div>
             </div>
           </>
         );
