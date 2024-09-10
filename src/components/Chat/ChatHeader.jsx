9183d00247e116d049f489dadc9cc4e7e02922b3
ZUITOPIA <korhwngml123@naver.com>
2024-09-10 21:36:13 +0900

    style: Header 스타일 적용

--- src/components/Chat/ChatHeader.jsx
+++ src/components/Chat/ChatHeader.jsx
@@ -1,3 +1,61 @@
+import styled from '@emotion/styled';
+
 export default function ChatHeader({ receiver }) {
-  return <div>{receiver?.userName}</div>;
+  return (
+    <>
+      <Style.Wrapper>
+        <Style.LeftWrapper>
+          <Style.Icon src="/left-arrow.png" alt="arrow" />
+
+          <Style.ReceiverProfile src="/dlwlrma.jpg" alt="profile" />
+
+          <Style.UserInfo>
+            <span>{receiver?.userName}</span>
+            <span> {receiver?.userId}</span>
+          </Style.UserInfo>
+        </Style.LeftWrapper>
+        <Style.RightWrapper>
+          <Style.Icon src="/alarm.png" alt="profile" />
+          <Style.IconVideo src="/video.png" alt="profile" />
+        </Style.RightWrapper>
+      </Style.Wrapper>
+    </>
+  );
 }
+
+const Style = {
+  Wrapper: styled.div`
+    display: flex;
+    justify-content: space-between;
+  `,
+  LeftWrapper: styled.div`
+    width: 140px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `,
+  RightWrapper: styled.div`
+    width: 70px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  `,
+  ReceiverProfile: styled.img`
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+    border: 1px solid #dbdbdb;
+  `,
+  Icon: styled.img`
+    width: 22px;
+    height: 22px;
+  `,
+  IconVideo: styled.img`
+    width: 32px;
+    height: 32px;
+  `,
+  UserInfo: styled.div`
+    display: flex;
+    flex-direction: column;
+  `,
+};
