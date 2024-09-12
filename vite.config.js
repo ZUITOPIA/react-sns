b3ecef0af87ca3d6cec65c198c4276cbe5dc383c
ZUITOPIA <korhwngml123@naver.com>
2024-09-12 16:25:37 +0900

    fix: 빌드에러 수정중

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: ['@emotion/babel-plugin'], // Emotion Babel 플러그인 추가
-      },
-    }),
-  ],
+  plugins: [react()],
   server: {
     port: 3000,
     open: true,
   },
-  define: { global: {} },
 });
